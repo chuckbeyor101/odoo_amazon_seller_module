ccc0d9c526705a2d73dce4084f880a631a485947
Chuck <8863436+chuckbeyor101@users.noreply.github.com>
2025-06-29 08:34:40 -0400

    Add FBA inventory ledger model and cron

--- __manifest__.py
+++ __manifest__.py
@@ -10,6 +10,8 @@
     'data': [
         'security/ir.model.access.csv',
         'views/amazon_seller_account_views.xml',
+        'views/amazon_fba_inventory_views.xml',
+        'data/cron.xml',
     ],
     # Install the python-amazon-sp-api package when the module is installed
     'post_init_hook': '_install_python_dependencies',
